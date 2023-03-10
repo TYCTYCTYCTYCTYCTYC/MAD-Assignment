26484994e6086b81154a89a717479c55422006cd
Tan Yi Chun <yctan2000@1utar.my>
2023-03-10 18:42:10 +0800

    event handling for clicking grids, some UI changes and changed app name

--- lib/main.dart
+++ lib/main.dart
@@ -34,14 +34,31 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   int active_index = 0;
+  int grid_count = 5;
+  int cur_index = 0;
+  int score = 0;
+  int round = 1;
+  double active_border = 5.0;
+  Random random = Random();
+
+  late int rng = random.nextInt(grid_count * grid_count);
+  late List<double> borders =
+      List.generate(grid_count * grid_count, (index) => 0);
+
+  @override
+  void initState() {
+    super.initState();
+    // final int rng = Random().nextInt(grid_count * grid_count);
+    borders[rng] = active_border;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: const Icon(Icons.menu, size: 50),
-        title: const Text('HomeSHARE', style: TextStyle(fontSize: 30)),
-        actions: const [Icon(Icons.account_circle, size: 50)],
+        // leading: const Icon(Icons.menu, size: 50),
+        title: const Text('game', style: TextStyle(fontSize: 30)),
+        // actions: const [Icon(Icons.account_circle, size: 50)],
         centerTitle: true,
         backgroundColor: Colors.blue,
       ),
@@ -49,26 +66,58 @@ class _HomeState extends State<Home> {
         reverse: true,
         child: Column(
           children: <Widget>[
-            Image.asset('assets/images/tio.png'),
+            // Image.asset('assets/images/tio.png'),
+            Text(
+              'score: ' + score.toString(),
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              'rng: ' + rng.toString(),
+            ),
+            Text(
+              'round: ' + round.toString(),
+            ),
             SizedBox(
               height: max(MediaQuery.of(context).size.height,
                   MediaQuery.of(context).size.width),
               child: GridView.count(
-                physics: NeverScrollableScrollPhysics(),
-                crossAxisCount: 3,
-                children: List.generate(9, (index) {
-                  return Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 2.0,
-                      ),
-                    ),
-                    child: Center(
-                      child: Text(
-                        'Box ${index + 1}',
-                        style: TextStyle(fontSize: 20.0),
-                      ),
+                physics: const NeverScrollableScrollPhysics(),
+                crossAxisCount: grid_count,
+                children: List.generate(grid_count * grid_count, (index) {
+                  return Material(
+                    child: InkWell(
+                      onTap: () {
+                        setState(() {
+                          if (index == rng) {
+                            score++;
+                          } else {
+                            score = max(score - 1, 0);
+                          }
+                          borders[rng] = 0;
+                          int tmp =
+                              Random().nextInt(grid_count * grid_count - 1);
+                          if (tmp >= rng)
+                            rng = tmp + 1;
+                          else
+                            rng = tmp;
+                          borders[rng] = active_border;
+                          round++;
+                        });
+                      },
+                      child: ClipRRect(
+                          //borderRadius: BorderRadius.circular(20.0),
+                          child: Container(
+                        decoration: BoxDecoration(
+                          image: const DecorationImage(
+                            image: AssetImage('assets/images/tio.png'),
+                            fit: BoxFit.cover,
+                          ),
+                          border: Border.all(
+                            color: Colors.black,
+                            width: borders[index],
+                          ),
+                        ),
+                      )),
                     ),
                   );
                 }),
