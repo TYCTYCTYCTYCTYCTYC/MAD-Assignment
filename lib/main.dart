585986c87b562cfecec23e4a91e7ef60bc8d0ee3
Tan Yi Chun <yctan2000@1utar.my>
2023-03-19 01:01:24 +0800

    removed test features and to do list

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:mad_ass/home.dart';
 import 'package:mad_ass/leaderboard.dart';
 
-//testing purposes, test = true uses smaller testing values of min_level, max_level, top and _countdownTime
-//else use actual values
-const bool test = false;
-
 bool insertScore = false;
 bool played = false;
 bool enter = false;
 int total_score = 0;
 int level = min_level;
-final int min_level =
-    (!test) ? 1 : 1; //technically toggleable but recommended to be 1
-final int max_level = (!test) ? 5 : 3; //toggleable parameter
-final int top = (!test) ? 25 : 3; //toggleable parameter
+final int min_level = 1; //technically toggleable but recommended to be 1
+final int max_level = 5; //toggleable parameter
+final int top = 25; //toggleable parameter
 List<int> score_list = [];
-final double startingCountdownTime = (!test) ? 5.0 : 3.0; //toggleable parameter
+final double startingCountdownTime = 5.0; //toggleable parameter
 String? name = null;
 
 void main() async {
